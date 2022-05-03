a06894ab3ddd38fe92ebc76e186f14a68602f50e
Zen <zunicoco@163.com>
2022-05-03 20:33:10 +0800

    feat: 公共信池发信 controller & router

--- server/controller/letter.js
+++ server/controller/letter.js
@@ -6,7 +6,7 @@
 const Letter = require("../models/letter");
 const User = require("../models/user");
 const { SuccessModel, ErrorModel } = require("../utils/res_model");
-const { stringReqT, stringReqF } = require("./config/paramsVerifyList")
+const { stringReqT } = require("./config/paramsVerifyList")
 
 const LetterType = {
     self_status: 0,
@@ -45,17 +45,6 @@ class LetterCtrl {
             })
         })()
 
-        // const letter = await new Letter({
-        //     ...ctx.request.body,
-        //     owner: _id,
-        //     mode: LetterType.self_status,
-        //     status: Math.round(Math.random())
-        // })
-        // .save()
-
-        // const user = await User.findById(_id).select('+letters')
-        // user.letters.push(letter._id)
-        // user.save()
         ctx.body = new SuccessModel("信件已往未来～")
     }
 
@@ -85,6 +74,40 @@ class LetterCtrl {
            console.log(err)
            ctx.body = new ErrorModel("信件寄送失败，请稍后再试!")
        }
+    }
+
+    async postPoolPublic(ctx, next) {
+        ctx.verifyParams({
+            title: stringReqT,
+            context: stringReqT
+        })
+
+        const _id = ctx.state.user._id
+
+        try {
+            (async() => {
+                ctx.body = await new Promise((resolve, reject) => {
+                    setTimeout(async () => {
+                        const letter = await new Letter({
+                            ...ctx.request.body,
+                            owner: _id,
+                            mode: LetterType.pool_public,
+                            status: Math.round(Math.random())
+                        })
+                        .save()
+    
+                        const user = await User.findById(_id).select('+letters')
+                        user.letters.push(letter._id)
+                        user.save()
+                        resolve(letter)
+                    }, 10000)
+                })
+            })()
+
+            ctx.body = new SuccessModel("信件已寄往公共信池~")
+        } catch (err) {
+           ctx.body = new ErrorModel("信件寄送失败，请稍后再试!")
+        }
     }
 }
 
--- server/routes/letter.js
+++ server/routes/letter.js
@@ -5,12 +5,13 @@
 
 const router = require('koa-router')()
 
-const { postSelfStatus, postSelfDate } = require('../controller/letter')
+const { postSelfStatus, postSelfDate, postPoolPublic } = require('../controller/letter')
 const { auth } = require('../middleware/auth')
 
 router.prefix('/letter')
 
 router.post('/self/status', auth, postSelfStatus)
 router.post('/self/date', auth, postSelfDate)
+router.post('/pool/public', auth, postPoolPublic)
 
 module.exports = router
